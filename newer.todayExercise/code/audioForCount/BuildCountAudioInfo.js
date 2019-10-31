8fbb7761ad29211ed4a33dec1a584cbe8052a3cf
ssyup4259 <30275948+ssyup4259@users.noreply.github.com>
2019-10-31 15:17:25 +0900

    새로운 폴더 만들었는데 안씀

---
+++ newer.todayExercise/code/audioForCount/BuildCountAudioInfo.js
@@ -0,0 +1,33 @@
+module.exports.function = function buildCountAudioInfo(countAudio) {
+  var audioInfo = {};
+  /* 오디오 인포 만드는 곳임 */
+  audioInfo.audioItem = countAudio.map(function (audioItem) {
+    let audioItemStructure = { // 오디오 아이템에 필요한 속성들
+      id: audioItem.id,
+      stream: audioItem.stream,
+      title: audioItem.title,
+      artist:audioItem.artist,
+      albumArtUrl: audioItem.albumArtUrl,
+      duration: audioItem.duration
+    } 
+
+    //오디오 아이템의 추가 설정하는 부분
+    if (audioItem.subtitle) {
+      audioItemStructure.subtitle = audioItem.subtitle
+    }
+    if (audioItem.albumName) {
+      audioItemStructure.albumName = audioItem.albumName
+    }
+    if (audioItem.duration) {
+      audioItemStructure.duration = audioItem.duration
+    }
+    return audioItemStructure
+  });
+
+  audioInfo.category = 'RADIO';
+  audioInfo.displayName = 'Count Capsule';
+  audioInfo.repeatMode = 'OFF';  //오디오 반복모드
+  audioInfo.doNotWaitForTTS = false;  //오디오가 빅스비의 말을 끝나고 할지(false) 빅스비말이랑 같이 시작할지(true)
+  return audioInfo;
+}
+
---
+++ newer.todayExercise/code/recommend/saveRoutine.js
@@ -0,0 +1,25 @@
+module.exports.function = function saveRoutine ($vivContext,routine) {
+  var secret = require('secret');
+  var apikey = secret.get('apikey');
+  let console = require('console');
+  var config = require('config');
+  var baseUrl=config.get('baseUrl');
+  let options = {
+    format: 'json',
+    cacheTime: 0,
+    headers: {
+      'X-API-Key': apikey
+    }
+  };
+  let http = require('http');
+  
+
+  //insert rocord
+  action = "insertRouRecord";
+  let user_id = "&user_id="+$vivContext.bixbyUserId;
+  var routine_id = "&routine_id=" + routine.routineNum;
+  http.getUrl(baseUrl+action+user_id+routine_id ,options );
+
+  console.log(baseUrl+action+user_id+routine_id ,options );
+  return routine;
+}
